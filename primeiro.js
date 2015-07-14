9f0ae3502b45ff836c762bb932fa3721300414ab
System Administrator <root@MacBook-Pro-2.local>
2015-07-14 13:33:09 -0300

    Comentários

--- primeiro.js
+++ primeiro.js
@@ -1,8 +1,13 @@
+
+//Objeto da API Webpage
 var page=require('webpage').create();
 
+//Função que faz a requisição
 page.open("http://exame.abril.com.br", function(status){
-
+	
+	//Valida status da requisição
 	if (status == "success"){
+		//Imprime o titulo do objeto page carregado
 		console.log(page.title);
 	} else {
 		console.log("Erro")
