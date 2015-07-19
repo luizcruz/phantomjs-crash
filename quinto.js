32c5f3ae4d55cf5729e8a6aededdc07173b8c0c2
System Administrator <root@MacBook-Pro-2.local>
2015-07-18 23:46:19 -0300

    adição de comentários

--- quinto.js
+++ quinto.js
@@ -1,17 +1,22 @@
+//Objetos das APIs Webpage e Filesystem
 var page = require('webpage').create();
 var fs = require('fs');
+
+//Arquivo de destino (formato JSON)
 
 var filePath = 'megasena.json';
 
 page.open('http://www1.caixa.gov.br/loterias/resultados/asp/megasena.asp', function(status) {
 
+	//Avalia funções no contexto da página
 	var json = page.evaluate(function() {
 		var items=document.getElementsByClassName("numbers mega-sena").item(0).getElementsByTagName("li")
 		var numbers="";
 		for (var j = 0; j < items.length; ++j) {
 			numbers += " "+items[j].innerText;
 	        }
-
+		
+		//Retorna objeto no padrão JSON
 		return {
 			description: document.getElementsByTagName('h2').item(3).getElementsByTagName('span').item(0).innerText,
 			result: document.getElementsByClassName("resultado-loteria").item(0).getElementsByClassName("epsilon").item(0).innerText,
@@ -22,6 +27,7 @@ page.open('http://www1.caixa.gov.br/loterias/resultados/asp/megasena.asp', funct
 
   	//console.log(JSON.stringify(json,null,5));
 
+	//Grava arquivo JSON
 	var out = fs.open(filePath, 'w');
 	out.write(JSON.stringify(json,null,5));
 	out.close();
